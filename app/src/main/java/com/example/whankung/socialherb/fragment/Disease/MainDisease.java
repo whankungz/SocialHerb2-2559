8893c9a36d15b44ab89e09abc3ff4915490b1015
Whan <sweet.whankungz@hotmail.com>
2017-01-29 16:57:26 +0700

    add data disease

--- app/src/main/java/com/example/whankung/socialherb/fragment/Disease/MainDisease.java
+++ app/src/main/java/com/example/whankung/socialherb/fragment/Disease/MainDisease.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,6 +16,9 @@ import android.widget.TextView;
 
 import com.example.whankung.socialherb.R;
 import com.example.whankung.socialherb.fragment.Herb.SearchHerb;
+
+import static com.example.whankung.socialherb.R.id.search;
+import static com.example.whankung.socialherb.R.id.searchD;
 
 /**
  * Created by Whankung on 16/1/2560.
@@ -32,16 +37,54 @@ public class MainDisease extends android.support.v4.app.Fragment{
     }
 
     private void setView() {
-        EditText searchD = (EditText)rootView.findViewById(R.id.searchD);
-        searchD.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-            @Override
-            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-                if (actionId == EditorInfo.IME_ACTION_DONE) {
+        final EditText searchD = (EditText)rootView.findViewById(R.id.searchD);
 
-                    FragmentManager m =getFragmentManager();
-                    FragmentTransaction t =m.beginTransaction();
-                    t.replace(R.id.frgMain, new SearchDisease());
-                    t.commit();
+        searchD.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void afterTextChanged(Editable arg0) {
+
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+
+                searchD.setImeOptions(EditorInfo.IME_ACTION_DONE);
+                String strMsg = "ปวดฟัน";
+                if (s.toString().equals(strMsg)) {
+                    searchD.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+                        @Override
+                        public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+
+                            if (actionId == EditorInfo.IME_ACTION_DONE) {
+
+                                FragmentManager m = getFragmentManager();
+                                FragmentTransaction t = m.beginTransaction();
+                                t.replace(R.id.frgMain, new  SearchDisease());
+                                t.commit();
+
+                            }
+                            return false;
+                        }
+                    });
+                }
+            }
+        });
+    }
+//        searchD.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+//            @Override
+//            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+//                if (actionId == EditorInfo.IME_ACTION_DONE) {
+//
+//                    FragmentManager m =getFragmentManager();
+//                    FragmentTransaction t =m.beginTransaction();
+//                    t.replace(R.id.frgMain, new SearchDisease());
+//                    t.commit();
 
 //                    FragmentManager manager = getSupportFragmentManager();
 //                    FragmentTransaction transaction = manager.beginTransaction();
@@ -49,9 +92,9 @@ public class MainDisease extends android.support.v4.app.Fragment{
 //                    transaction.commit();
 //                    SearchHerb();
 //                    return; true;
-                }
-                return false;
-            }
-        });
-    }
+//                }
+//                return false;
+//            }
+//        });
+//    }
 }
--- app/src/main/java/com/example/whankung/socialherb/fragment/Disease/SearchDisease.java
+++ app/src/main/java/com/example/whankung/socialherb/fragment/Disease/SearchDisease.java
@@ -1,6 +1,7 @@
 package com.example.whankung.socialherb.fragment.Disease;
 
 import android.graphics.Typeface;
+import android.media.Rating;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.TabLayout;
@@ -9,6 +10,7 @@ import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RatingBar;
 import android.widget.TextView;
 
 import com.example.whankung.socialherb.R;
@@ -24,30 +26,35 @@ import java.util.List;
  * Created by Whankung on 22/1/2560.
  */
 
-public class SearchDisease extends Fragment{
+public class SearchDisease extends Fragment {
     private View rootView;
     private TabLayout tabLayout;
     private Typeface font;
+    private TextView t, t2, t3, t4, t5, t6, t7, t8, t9;
+    private RatingBar r;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceStat) {
         rootView = inflater.inflate(R.layout.search_disease, container, false);
         setView();
+        setSearch();
 
         return rootView;
     }
 
+
     private void setView() {
-        TextView t = (TextView) rootView.findViewById(R.id.nameD);
-        TextView t2= (TextView) rootView.findViewById(R.id.nameDdata);
-        TextView t3 = (TextView) rootView.findViewById(R.id.nameOther);
-        TextView t4 = (TextView) rootView.findViewById(R.id.nameOtherda);
-        TextView t5 = (TextView) rootView.findViewById(R.id.namewhy);
-        TextView t6 = (TextView) rootView.findViewById(R.id.namewhyda);
-        TextView t7= (TextView) rootView.findViewById(R.id.namehow);
-        TextView t8 = (TextView) rootView.findViewById(R.id.namehowda);
-        TextView t9 = (TextView) rootView.findViewById(R.id.submit);
+        t = (TextView) rootView.findViewById(R.id.nameD);
+        t2 = (TextView) rootView.findViewById(R.id.nameDdata);
+        t3 = (TextView) rootView.findViewById(R.id.nameOther);
+        t4 = (TextView) rootView.findViewById(R.id.nameOtherda);
+        t5 = (TextView) rootView.findViewById(R.id.namewhy);
+        t6 = (TextView) rootView.findViewById(R.id.namewhyda);
+        t7 = (TextView) rootView.findViewById(R.id.namehow);
+        t8 = (TextView) rootView.findViewById(R.id.namehowda);
+        t9 = (TextView) rootView.findViewById(R.id.submit);
+        r = (RatingBar) rootView.findViewById(R.id.rat);
         font = Typeface.createFromAsset(getContext().getAssets(), "tmedium.ttf");
 
         t.setTypeface(font);
@@ -59,9 +66,13 @@ public class SearchDisease extends Fragment{
         t7.setTypeface(font);
         t8.setTypeface(font);
         t9.setTypeface(font);
+    }
 
-
-
+    private void setSearch() {
+        t2.setText("ปวดฟัน");
+        t4.setText("กานพลู ดาวเรือง เมล็ดผักชี ผักบุ้งนา มะระ ว่านหางจระเข้ น้ำมันกระเทียม ใบชา เมล็ดกุยช่าย");
+        t6.setText("เวลากินของเย็น ของหวาน หรือเมื่อตอนเคี้ยวอาหารและอาการเหล่านี้จะหายไปเมื่อหยุดกินอาหารดังกล่าวภายในไม่กี่นาึ่งลักษณะของอาการปวดเช่นนี้มักเกิดจากฟันผุหรือฟันบิ่นจนถึงเนื้อฟันชั้นในจึงทำให้ความเย็นหรือแรงจากการเคี้ยวอาหารมีโอกาสกระตุ้นเส้นประสาทที่อยู่ในโพรงประสาทใต้เนื้อฟันได้มากกว่าปกติ จึงทำให้เกิดความรู้สึกเสียวฟันทุกครั้งเมื่อกินอาหาร ส่วนอาการปวดฟันอีกประเภทหนึ่ง ซึ่งจะมีความรุนแรงกว่า คือ การปวดเป็นจังหวะ ตุ้บ ๆ ซึ่งอาจปวดโดยอยู่เฉย ๆ ก็ปวด หรืออาจปวดมากขึ้นเวลากินของเย็นหรือของร้อน หรือเวลาเคี้ยวอาหาร และอาการปวดนี้จะไม่หายไปแม้จะเลิกกินอาหารเหล่านี้แล้วก็ตาม");
+        t8.setText("หลีกเลี่ยงอาหารประเภทดังกล่าวพร้อมกับการรับประทานยาแก้ปวด");
 
     }
 }
